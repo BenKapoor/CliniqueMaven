be2a17d33d6ea7a313b74b13f3d26bd1d24e7337
lgu <lorris97@hotmail.fr>
2019-11-18 12:18:31 +0100

    Creation du projet

---
+++ CliniqueMaven/CliniqueMavenSERVICE/src/main/java/eu/ensup/cliniquemavenservice/AuthentificationService/AuthentificationService.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.ensup.cliniquemavenservice.AuthentificationService;
+
+/**
+ *
+ * @author lorris
+ */
+public class AuthentificationService {
+    
+    public boolean identification(String identifiant, String password,MedecinBO bo)
+    {
+        boolean b = false; 
+        
+        
+        
+        
+        return b;
+    }
+    
+}
