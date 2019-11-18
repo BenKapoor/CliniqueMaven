a24dca43a1e83fde5711ccf340c0148795e0ed72
BenKapoor <benjaminkapoor@yahoo.fr>
2019-11-18 13:14:31 +0100

    commit

--- CliniqueMaven/CliniqueMavenDAO/src/main/java/eu/ensup/cliniquemavendao/ConnectionDao.java
+++ CliniqueMaven/CliniqueMavenDAO/src/main/java/eu/ensup/cliniquemavendao/ConnectionDao.java
@@ -5,6 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * @author benja
+ *
+ */
 public class ConnectionDao {
 	private static Connection connect = null;
 
