4b5d4ab2b24b25656ea0539626a9468fb5057e66
BenKapoor <benjaminkapoor@yahoo.fr>
2019-11-19 15:28:22 +0100

    test mockiot junit

---
+++ CliniqueMaven/CliniqueMavenDAO/src/main/test/eu/ensup/cliniquemavendao/test/PatientDaoTest.java
@@ -0,0 +1,43 @@
+package eu.ensup.cliniquemavendao.test;
+
+import org.graalvm.compiler.debug.Assertions;
+
+import eu.ensup.cliniquemavendao.IPatientDao;
+import eu.ensup.cliniquemavenservice.PatientService;
+import eu.ensup.cliniqueservicebo.Patient;
+
+//@InjectMock
+public class PatientDaoTest extends TestCase {
+	// 1- Mocker l'interface du dao
+	@Mock
+	IPatientDao dao;
+
+	// 2- Injecte le service
+	@InjectMocks
+	PatientService service;
+
+	@Test
+	public void testLectureInfoPatient() {
+		// Permet d'initialiser l'objet mock
+		MockitoAnnotations.initMocks(this);
+		/**
+		 * Sénario de test : La demande de lecture de l'information d'un médecin à
+		 * partir de son identifiant nous ramène la totalité des informations de ses
+		 * caractéristiques
+		 */
+
+		// 3- Imposer un comportement au mock(stubber)
+		// @Spy
+		Patient spiedMedecin = new Patient();
+		Mockito.when(this.dao.getPatientById(1)).thenReturn(spiedMedecin);
+
+		// 4- Appeler une méthode du service
+		Patient patient = service.lireUnPatient(1);
+
+		// 5- Comparer le résultat réel avec le résultat attendu
+		Assertions.assertEquals(patient.getId(), 1);
+
+		// verifier l'appel et le nombre de fois
+		Mockito.verify(dao, Mockito.times(1)).getMedecinById(1);
+	}
+}
